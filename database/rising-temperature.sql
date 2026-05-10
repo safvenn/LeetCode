9fd7530be1d6eb2603fc6f1d5a26fd706f2e19ed
Safvan sidheeq <mkdsafwan4@gmail.com>
2026-05-10 18:04:26 +0530

    LC-rising-temperature [Runtime: N/A; Memory: N/A]

--- database/rising-temperature.sql
+++ database/rising-temperature.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
 with temp as(
-select id,(lag(temperature) over(order by recordDate)) t,temperature from weather
+select id,(lag(temperature) over(order by recordDate)) t,temperature from weather order by recordDate
 )
 select id from temp where temperature > t
